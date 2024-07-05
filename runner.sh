fe17771e248e1a2763285cf3d6e9d74dd11116de
Eric Christensen <hebrew.8.12@gmail.com>
2024-07-05 11:09:27 -0700

    more updates to script

--- runner.sh
+++ runner.sh
@@ -3,4 +3,4 @@
 SCRIPT_DIR=$(dirname "$0")
 LOG_FILE="$SCRIPT_DIR/ddns.log"
 
-$HOME/.deno/bin/deno run -A $SCRIPT_DIR/ddns_cloudflare.ts >> "$LOG_FILE" 2>&1 &
+deno run -A $SCRIPT_DIR/ddns_cloudflare.ts >> "$LOG_FILE" 2>&1 &
